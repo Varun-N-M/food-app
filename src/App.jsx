acf4253563ea62d56d1b3bb4417b3269aeb109e9
Varun <varunnm68@gmail.com>
2023-09-22 20:36:29 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { Hero } from './components/Hero'
 import { HeadLineCards } from './components/HeadlineCards'
 import { Food } from './components/Food'
 import { Category } from './components/Category'
+import { Analytics } from '@vercel/analytics/react';
 
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <HeadLineCards/>
-      <Food/>
-      <Category/>
+      <Navbar />
+      <Hero />
+      <HeadLineCards />
+      <Food />
+      <Category />
+      <Analytics />
     </div>
   )
 }
